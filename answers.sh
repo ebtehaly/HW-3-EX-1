98c7b7e9b8e8993e0236fe4c34b78cd04f381e2a
ebtehaly <158605735+ebtehaly@users.noreply.github.com>
2024-10-09 19:23:08 -0500

    Added myself to the list of students

--- answers.sh
+++ answers.sh
@@ -2,7 +2,8 @@ SELECT Name FROM Students;
 SELECT * FROM Students WHERE Age > 30;
 SELECT Name FROM Students WHERE Gender='F' AND Age=30;
 SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
+INSERT INTO Students VALUES(7, 'Ebtehal', '22', 'F', 700);
+SELECT * FROM Students WHERE ID = 7;
 UPDATE Students SET Points = 500 WHERE Name='Basma';
 UPDATE Students SET Points = 100 WHERE Name='Alex';
 
